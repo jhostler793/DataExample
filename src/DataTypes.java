fd8339e3717b7eb7dba7a3a42bfc7c622b9b1cfc
jhostler793 <jmh12@hood.edu>
2020-08-24 16:05:24 -0400

    DataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,39 @@
+public class DataTypes {
+    public static void main (String[] args){
+        // just declaring a boolean variable, no value
+       boolean isRaining;
+       // declaring and initializing (giving it a value)
+        boolean isSunny = false;
+        // byte is quite limited. mostly for bebugging purposes
+        byte b = 0;
+
+        int myNumber=239;
+
+        double myDoubleVariable = 0.5;
+
+        char singleCharacter = 'y';
+
+        String myFirstName = "Jamie";
+
+        String myLastName = "Hostler";
+
+        System.out.println("My first name is Rey");
+
+        System.out.println("First value of singleCharacter: " + singleCharacter);
+
+        singleCharacter = 'a';
+
+        System.out.println("Second value of singleCharacter:" + singleCharacter);
+
+        System.out.println("Is it sunny?" + isSunny);
+
+        System.out.println("My number variable plus 1 is : " + (myNumber + 1));
+
+
+
+
+
+
+
+    }
+}
